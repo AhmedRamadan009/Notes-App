a6a836f89cb1ed1b98e925725b4c14fc8f61cf26
AhmedRamadan009 <105159451+AhmedRamadan009@users.noreply.github.com>
2023-01-08 14:17:54 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-<BrowserRouter>
+<HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
   </>
 );
 
